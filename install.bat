0e4f760ee566c029af24332ef7140f0c0bec8636
UacProgrammer <54560890+UacProgrammer@users.noreply.github.com>
2022-08-10 11:35:10 -0500

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+echo Instalador de la base de datos Universidad
+echo Autor: Abraham Arteaga Lopez
+echo 9 de agosto del 2022
+sqlcmd -S. -E -i BDUniversidad2.sql
+echo Se ejecuto correctamente la base de datos
+pause
